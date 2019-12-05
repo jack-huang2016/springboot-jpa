d7504d3af92f9b5064a0cdc109ef050f4859562a
jack-huang2016 <2456120975@qq.com>
2019-12-05 21:57:33 +0800

    Springboot整合JPA

---
+++ springboot-jpa-web/src/main/java/com/sample/test/SpringbootJpaWebApplication.java
@@ -0,0 +1,13 @@
+package com.sample.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaWebApplication.class, args);
+    }
+
+}
